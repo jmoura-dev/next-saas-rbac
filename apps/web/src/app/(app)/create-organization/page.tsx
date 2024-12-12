45318d1fe19fb18d9a53edcf22c1814e160a2ae1
Jackson <jackson1712@hotmail.com>
2024-12-11 22:02:57 -0300

    feat: add intercepting routes to create organization

--- apps/web/src/app/(app)/create-organization/page.tsx
+++ apps/web/src/app/(app)/create-organization/page.tsx
@@ -1,9 +1,13 @@
+import { Header } from '@/components/header'
+
 import { OrganizationForm } from './organization-form'
 
 export default function CreateOrganization() {
   return (
-    <div className="space-y-4">
+    <div className="mx-auto w-full max-w-[1200] space-y-4 py-4">
+      <Header />
       <h1 className="text-2xl font-bold">Create Organization</h1>
+
       <OrganizationForm />
     </div>
   )
